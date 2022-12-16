144da35046901785804da8891f0721fbd56592a1
shank927 <shankarer00384@gmail.com>
2022-12-16 19:26:49 +0545

    Variables in C#

--- project1/Test.cs
+++ project1/Test.cs
@@ -1,10 +1,20 @@
 ﻿class Test
 {
 
-    public static void Main1()
+    public static void Main()
     {
-
         Console.WriteLine("Hello world");
+        int x = 89;
+        if (x % 2 == 0)
+        {
+            Console.WriteLine(x +"Even");
+        }
+
+        else
+        {
+
+            Console.WriteLine(x+"odd");
+        }
     }
 
 
--- project1/variable.cs
+++ project1/variable.cs
@@ -1,8 +1,33 @@
 class Variables{
 
-public static void Main(){
-Console.WriteLine("Hello  ia am going to lea....");
+//numbers: integers floating point
+void LearnVariables(){
+byte b=255;
+short s=2345;
+int i=434343;
+long l=3545446466;
 
+float f=345.6754535353335f;
+double d=22324.5644646565;
+decimal deci=3545453533.55656565m;
+//Test
+
+string s1="Sky";
+char gender='M';
+
+
+//boolean 
+bool areYouMale=true;
+
+//Dates
+DateTime dob;
+Console.WriteLine("Byte="+b);
+Console.WriteLine("short="+s);
+Console.WriteLine("Integer="+i);
+Console.WriteLine("Long="+l);
+Console.WriteLine("float="+f);
+Console.WriteLine("double="+d);
+Console.WriteLine("Decimal="+deci);
 
 }
 
