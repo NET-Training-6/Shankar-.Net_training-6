33ff3d5770a9ecc9ed8b2f5cf5807e7517687172
shank927 <shankarer00384@gmail.com>
2022-12-18 20:39:29 +0545

    Number multiple of 3 and 5

--- project1/Test.cs
+++ project1/Test.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+
 class Test
 {
 
     public static void Main()
     {
-        Console.Write("Enter an amount:");
-        int amt = int.Parse(Console.ReadLine());
-        Console.Write("Enter a Time period:");
-        int ti = int.Parse(Console.ReadLine());
-        Console.Write("Enter interest interest:");
-         int rate = int.Parse(Console.ReadLine());
-        int si=amt*ti*rate/100;
-        Console.WriteLine("The simple interest is="+si);
-        
+        Console.Write("Enter an number:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        /* if ((x % 3 == 0 )&& (x % 5 == 0))
+         {
 
+             Console.WriteLine("The number" + x + "is multiple of 3 and 5");
+         }
+
+         else
+         {
+             Console.WriteLine("The number" + x + "is not  multiple of 3 and 5");
+         }
+     */
+
+        String result = ((x % 3 == 0) && (x % 5 == 0)) ? "The number is multiple of 3 and 5" : "The number is not  multiple of 3 and 5";
+
+        Console.WriteLine(result);
     }
 
 
